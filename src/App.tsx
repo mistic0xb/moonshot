6af305e4741468c30e995895d99f458b1f2cb922
Mist <mistic0xb@gmail.com>
2025-12-29 09:31:59 +0530

    added stateTime to tanstack-query

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,14 @@ import { ExportedMoonshotsProvider } from "./context/ExportedMoonshotContext";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 10 * 60 * 1000, // 10 minutes
+    },
+  },
+});
 
 function App() {
   return (
