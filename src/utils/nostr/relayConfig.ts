0e72d329ad0e756788d823ed6d31ef875dd7e761
Mist <mistic0xb@gmail.com>
2025-12-29 09:35:27 +0530

    new relay added

--- src/utils/nostr/relayConfig.ts
+++ src/utils/nostr/relayConfig.ts
@@ -1,6 +1,12 @@
 export const DEFAULT_RELAYS = [
   "wss://relay.angor.io/",
   "wss://relay2.angor.io",
+  "wss://relay.damus.io",
+];
+
+export const MSG_DEFAULT_RELAYS = [
+  "wss://relay.damus.io",
+  "wss://nostr.mom"
 ];
 
 export const MOONSHOT_FEE = 1000; // 1k sats
